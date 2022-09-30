5acf3c4c51d4be00aaa167f5035c0e3595c934ad
rakemeup <bencehetes@gmail.com>
2022-09-30 16:10:06 +0200

    not done yet

---
+++ src/utils/isScrollingUp.ts
@@ -0,0 +1,7 @@
+/* 
+    calling it twice will return if its scrolling up or not.
+    false, true => scrolling down
+    true, false => scrolling up
+*/
+
+export function isScrollingUp(intersectingStatus: boolean) {}
