96436f3ddc2f30647d0c50696c39a49eb8314c85
rakemeup <bencehetes@gmail.com>
2022-10-02 18:51:48 +0200

    extracted header for clarity

--- src/sections/TechStack.tsx
+++ src/sections/TechStack.tsx
@@ -1,14 +1,7 @@
-import { StyledHeader } from "../styles/shared.styles";
-
 type Props = {};
 
 const TechStack = () => {
-    return (
-        <StyledHeader>
-            <h1>tech</h1>
-            <h2>tech stuff</h2>
-        </StyledHeader>
-    );
+    return <div>tech</div>;
 };
 
 export default TechStack;
